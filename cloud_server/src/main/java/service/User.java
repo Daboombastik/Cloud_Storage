9fd1b4ec763f761610570acb418edec3071a48b8
Daboombastik <s.shcherbakov@yahoo.fr>
2020-08-17 15:10:13 +0200

    main commit

---
+++ cloud_server/src/main/java/service/User.java
@@ -0,0 +1,28 @@
+package service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class User {
+    String login;
+    String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ common/src/main/java/FileParametersListMessage.java
@@ -0,0 +1,48 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+/**
+ *  Класс для передачи сообщения со  списком {@link FileParameters} между сервером и клиеном
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class FileParametersListMessage extends AbstractMessage{
+
+	private static final long serialVersionUID = 122661028145564867L;
+	
+	private List<FileParameters> fileParameterList;
+
+	public FileParametersListMessage(String fileStorage) {
+		List<FileParameters> fileParameterList = null;
+		try {
+			fileParameterList = Files.list(Paths.get(fileStorage)).map(e -> new FileParameters(e)).collect(Collectors.toCollection(ArrayList::new));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.fileParameterList = fileParameterList;
+	}
+
+	public FileParametersListMessage() {
+
+	}
+
+
+	public List<FileParameters> getFileParameterList() {
+		return fileParameterList;
+	}
+
+	public void setFileParameterList(List<FileParameters> fileParameterList) {
+		this.fileParameterList = fileParameterList;
+	}
+}
